d7780f3b814ec097977b22298c0a56cd5bedc0a2
Rodrigo Jimenez <rodrigojimenez@Rodrigos-MacBook-Pro.local>
2015-07-28 19:35:37 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -19,5 +19,6 @@
 // put new methods here
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
     // this will get called every time the player touches the screen
+    [self addObstacle];
 }
 @end
